eb643a5be30072200525b910794fbdc16d932dd1
Arjie Cristobal <arjie_c82@yahoo.com>
2020-03-20 14:36:38 -0400

    Section 12: Pause, Debug Mode and Selectors

--- webdriverioFramework/wdio.conf.js
+++ webdriverioFramework/wdio.conf.js
@@ -7,6 +7,9 @@ if (process.env.SERVER === 'prod') {
     baseUrl = 'http://webdriveruniversity.com';
 }
 
+// $ DEBUG=true npm test -- --specs=webdriverUniversityTest
+var timeout = process.env.DEBUG ? 99999999 : 10000;
+
 exports.config = {
     
     //
@@ -137,7 +140,8 @@ exports.config = {
     // Options to be passed to Mocha.
     // See the full list at http://mochajs.org/
     mochaOpts: {
-        ui: 'bdd'
+        ui: 'bdd',
+        timeout: timeout
     },
     //
     // =====
