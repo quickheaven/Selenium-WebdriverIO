35c66948edf6172d8e4132fb02b12818fdc7ab4f
Arjie Cristobal <arjie_c82@yahoo.com>
2020-03-20 14:57:30 -0400

    Section 13: Targeting and Skipping Specific Test

--- webdriverioFramework/tests/ajaxClickTest.js
+++ webdriverioFramework/tests/ajaxClickTest.js
@@ -1,10 +1,11 @@
 describe('Test that the button is clickable once the Ajax loader completes loading', function () {
-    it('Attempt to click the button asap', function (done) {
+    it.skip('Attempt to click the button asap', function (done) {
         browser.url('/Ajax-Loader/index.html');
         browser.click('#button1');
     });
 
-    it('Attempt to click the button after 7 seconds', function (done) {
+    //it.only
+    it.('Attempt to click the button after 7 seconds', function (done) {
         browser.url('/Ajax-Loader/index.html');
         this.timeout(20000);
         browser.pause(7000);
