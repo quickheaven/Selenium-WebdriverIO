b33a12bf64f15f3a19fa604685e4b804b906d47c
Arjie Cristobal <arjie_c82@yahoo.com>
2020-03-20 15:06:19 -0400

    Section 14: CSS Extraction

--- webdriverioFramework/tests/ajaxClickTest.js
+++ webdriverioFramework/tests/ajaxClickTest.js
@@ -5,7 +5,7 @@ describe('Test that the button is clickable once the Ajax loader completes loadi
     });
 
     //it.only
-    it.('Attempt to click the button after 7 seconds', function (done) {
+    it('Attempt to click the button after 7 seconds', function (done) {
         browser.url('/Ajax-Loader/index.html');
         this.timeout(20000);
         browser.pause(7000);
