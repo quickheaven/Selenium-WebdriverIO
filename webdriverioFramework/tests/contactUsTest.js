b5c8a0b8f1425715a50fe73e7340ef66a123a16f
Arjie Cristobal <arjie_c82@yahoo.com>
2020-03-23 18:20:56 -0400

    Section 22: Advanced Reporting

--- webdriverioFramework/tests/contactUsTest.js
+++ webdriverioFramework/tests/contactUsTest.js
@@ -14,7 +14,7 @@ browser.addCommand("submitDataViaContactUsForm", function (firstName, lastName, 
     browser.setValue("[name='message']", comments);
   }
   browser.click("[type='submit']");
-});
+})
 
 beforeEach(function () {
   browser.url('/Contact-Us/contactus.html');
@@ -31,47 +31,45 @@ describe('Test Contact Us form WebdriverUni', function () {
 
   contactusDetails.forEach(function (contactDetail) {
     it('Should be able to submit a successful submission via contact us form', function (done) {
-      browser.submitDataViaContactUsForm('Joe', 'Blogs', contactDetail.email, contactusDetail.comment);
+      browser.submitDataViaContactUsForm('joe', 'Blogs', contactDetail.email, contactDetail.body);
 
       var successfulContactConfirmation = browser.isExisting('#contact_reply h1');
       expect(successfulContactConfirmation, 'Successful submission Message does not exist').to.be.true;
 
       var successfulSubmission = browser.getText('#contact_reply h1');
       expect(successfulSubmission).to.equal('Thank You for your Message!');
-    });
+    })
+  });
+
+  it('Should not be able to submit a successful submission via contact us form as all fields are required', function (done) {
+    browser.setValue("[name='first_name']", 'Mike');
+    browser.setValue("[name='last_name']", 'Woods');
+    browser.setValue("[name='email']", 'mike_woods@mail.com');
+    browser.click("[type='submit']");
+
+    var successfulContactConfirmation = browser.isExisting('#contact_reply h1');
+    expect(successfulContactConfirmation, 'Successful submission Message does not exist').to.be.false;
+  });
 
 
-    it('Should not be able to submit a successful submission via contact us form as all fields are required', function (done) {
-      browser.setValue("[name='first_name']", 'Mike');
-      browser.setValue("[name='last_name']", 'Woods');
-      browser.setValue("[name='email']", 'mike_woods@mail.com');
-      browser.click("[type='submit']");
+  it('Should not be able to submit a successful submission via contact us form as all fields are required', function (done) {
+    browser.setValue("[name='first_name']", 'Sarah');
+    browser.setValue("[name='email']", 'sarah_woods@mail.com');
+    browser.click("[type='submit']");
 
-      var successfulContactConfirmation = browser.isExisting('#contact_reply h1');
-      expect(successfulContactConfirmation, 'Successful submission Message does not exist').to.be.false;
-    });
+    var successfulContactConfirmation = browser.isExisting('#contact_reply h1');
+    expect(successfulContactConfirmation, 'Successful submission Message does not exist').to.be.false;
+  });
 
+  it('Should not be able to submit a successful submission via contact us form as all fields are required', function (done) {
+    browser.setValue("[name='first_name']", 'Jim');
+    browser.setValue("[name='last_name']", 'Jomes');
+    browser.click("[type='submit']");
 
-    it('Should not be able to submit a successful submission via contact us form as all fields are required', function (done) {
-      browser.setValue("[name='first_name']", 'Sarah');
-      browser.setValue("[name='email']", 'sarah_woods@mail.com');
-      browser.click("[type='submit']");
+    var errorText = browser.getText('body');
+    expect(errorText).to.include('Error: all fields are required');
 
-      var successfulContactConfirmation = browser.isExisting('#contact_reply h1');
-      expect(successfulContactConfirmation, 'Successful submission Message does not exist').to.be.false;
-    });
-
-    it('Should not be able to submit a successful submission via contact us form as all fields are required', function (done) {
-      browser.setValue("[name='first_name']", 'Jim');
-      browser.setValue("[name='last_name']", 'Jomes');
-      browser.click("[type='submit']");
-
-      var errorText = browser.getText('body');
-      expect(errorText).to.include('Error: all fields are required');
-
-      var errorText = browser.isVisible('body');
-      expect(errorText, 'Error message is not visible').to.be.true;
-    });
-
+    var errorText = browser.isVisible('body');
+    expect(errorText, 'Error message is not visible').to.be.true;
   });
 });
