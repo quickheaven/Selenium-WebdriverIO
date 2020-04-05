508bf6ea5bdd26da7eeb890e3dcc5bf92ca77cc7
Arjie Cristobal <arjie_c82@yahoo.com>
2020-04-04 22:00:44 -0400

    Section 21: Page Object Model (POM)

--- webdriverioFramework/tests/contactUsTest.js
+++ webdriverioFramework/tests/contactUsTest.js
@@ -5,72 +5,31 @@ beforeEach(function () {
 })
 
 describe('Test Contact Us form WebdriverUni', function () {
-  function setFirstName(firstName) {
-    return ContactUs_Page.firstName.setValue(firstName);
-  }
-
-  function setLastName(lastName) {
-    return ContactUs_Page.lastName.setValue(lastName);
-  }
-
-  function setEmailAddress(emailAddress) {
-    return ContactUs_Page.emailAddress.setValue(emailAddress);
-  }
-
-  function setComments(comments) {
-    return ContactUs_Page.comments.setValue(comments);
-  }
-
-  function clickSubmitButton() {
-    return ContactUs_Page.submitButton.click();
-  }
-
-  function confirmSuccessfulSubmission() {
-    var validateSubmissionHeader = browser.waitUntil(function () {
-      return ContactUs_Page.successfulSubmissionHeader.getText() == 'Thank You for your Message!'
-    }, 3000)
-    expect(validateSubmissionHeader, 'Successful Submission Message does not Exist!').to.be.true;
-  }
-
-  function confirmUnsuccessfulSubmission() {
-    var validateSubmissionHeader = browser.waitUntil(function () {
-      return ContactUs_Page.unsuccessfulSubmissionHeader.getText() == 'Error: all fields are required'
-    }, 3000)
-    expect(ContactUs_Page.unsuccessfulSubmissionHeader.getText()).to.include('Error: all fields are required');
-  }
-
-
-
   it('Should be able to submit a successful submission via contact us form', function (done) {
-    setFirstName('joe');
-    setLastName('Blogs');
-    setEmailAddress('joe_blogs123@outlook.com');
-    setComments('How are you?');
-    clickSubmitButton();
-    confirmSuccessfulSubmission();
+    ContactUs_Page.submitAllInformationViaContactUsForm('joe', 'Blogs', 'joe_blogs123@outlook.com', 'How are you?');
   });
 
 
   it('Should not be able to submit a successful submission via contact us form as all fields are required', function (done) {
-    setFirstName('Mike');
-    setLastName('Woods');
-    setEmailAddress('mike_woods@mail.com');
-    clickSubmitButton();
-    confirmUnsuccessfulSubmission();
+    ContactUs_Page.setFirstName('Mike');
+    ContactUs_Page.setLastName('Woods');
+    ContactUs_Page.setEmailAddress('mike_woods@mail.com');
+    ContactUs_Page.clickSubmitButton();
+    ContactUs_Page.confirmUnsuccessfulSubmission();
   });
 
 
   it('Should not be able to submit a successful submission via contact us form as all fields are required', function (done) {
-    setFirstName('Sarah');
-    setEmailAddress('sarah_woods@mail.com');
-    clickSubmitButton();
-    confirmUnsuccessfulSubmission();
+    ContactUs_Page.setFirstName('Sarah');
+    ContactUs_Page.setEmailAddress('sarah_woods@mail.com');
+    ContactUs_Page.clickSubmitButton();
+    ContactUs_Page.confirmUnsuccessfulSubmission();
   });
 
   it('Should not be able to submit a successful submission via contact us form as all fields are required', function (done) {
-    setLastName('Jomes');
-    setEmailAddress('sarah_Jomes@mail.com');
-    clickSubmitButton();
-    confirmUnsuccessfulSubmission();
+    ContactUs_Page.setLastName('Jomes');
+    ContactUs_Page.setEmailAddress('sarah_Jomes@mail.com');
+    ContactUs_Page.clickSubmitButton();
+    ContactUs_Page.confirmUnsuccessfulSubmission();
   });
 });
