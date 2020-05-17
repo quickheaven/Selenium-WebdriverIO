4087ce4d7c9b321e5ac7f578cabf01fa8967e29f
Arjie Cristobal <arjie_c82@yahoo.com>
2020-05-17 15:14:24 -0400

    Section 46: Test Creation - Challenge!

---
+++ webdriverio-v5/test/contact-us-test.js
@@ -0,0 +1,31 @@
+describe("Test contact us page on webdriveruni", () => {
+  beforeEach(function () {
+    browser.setWindowSize(1800, 1200);
+    browser.url("./");
+
+    const pageDetails = browser.getUrlAndTitle();
+    expect(pageDetails.url).to.contain("webdriveruniversity");
+    expect(pageDetails.title).to.contain("WebDriverUniversity");
+  });
+
+  it("Submit all information via the contact us page", () => {
+    const contactUsButton = $('//h1[text()="CONTACT US"]/..');
+    contactUsButton.click();
+
+    browser.switchWindow("WebDriver | Contact Us");
+    const firstName = $('//*[@name="first_name"]');
+    const lastName = $('//*[@name="last_name"]');
+    const emailAddress = $('//*[@name="email"]');
+    const message = $('//*[@name="message"]');
+    const submitButton = $('//*[@value="SUBMIT"]');
+
+    firstName.setValue("Joe");
+    lastName.setValue("Blogs");
+    emailAddress.setValue("joe_Blogs121@mail.com");
+    message.setValue("Hello how are you?");
+    submitButton.click();
+
+    const contactUsSubmissionDetails = browser.getUrlAndTitle();
+    expect(contactUsSubmissionDetails.url).to.contain("contact-form-thank-you");
+  });
+});
